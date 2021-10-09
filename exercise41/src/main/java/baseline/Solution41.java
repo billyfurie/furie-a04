658af5b15865005acc5aaf310983472e99a97e2f
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-10-09 13:10:50 -0400

    Had to reupload skeleton code since I originally forgot to include my gitignore file

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,39 @@
+package baseline;
+
+import java.util.List;
+
+public class Solution41 {
+
+    public static void main(String[] args) {
+        // read list of names from file, creating a name object for each name
+        // sort the list of names in alphabetical order by last name
+        // write the sorted list to a file
+
+        // unit testing
+            // test list being sorted
+            // test the output file for correctness
+    }
+
+    private List<Name> getListOfNamesFromFile() {
+        // open and parse through the file
+        // each name is comma separated (first and last), linebreak for each name
+        // create name objects for each name
+        // add name objects to a list
+        // return list of names
+
+        // placeholder
+        return null;
+    }
+
+    private List<Name> getSortedListOfNamesByLastName(List<Name> names) {
+        // sort the list of names by last name
+        // return the sorted list
+
+        // placeholder
+        return null;
+    }
+
+    private void writeListToFile(List<Name> names) {
+        // go through the list, writing each name on a line in the file
+    }
+}
