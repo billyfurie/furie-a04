d32da810a5b186babb93be020cf41f9f47316ba4
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-10-12 13:48:45 -0400

    Finished skeleton code and diagram for exercise44

---
+++ exercise44/src/main/java/baseline/Product.java
@@ -0,0 +1,20 @@
+package baseline;
+
+public class Product {
+
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        // set all the instance variables
+    }
+
+    @Override
+    public String toString() {
+        // return the product as a string in our specified format
+
+        // placeholder
+        return null;
+    }
+}
---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,39 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+public class Solution44 {
+
+    public static void main(String[] args) {
+        // keep getting product name from user
+        // search for product in inventory
+        // if it is in inventory, display the info, otherwise prompt for a different product
+    }
+
+    private String getProductNameFromUser() {
+        // prompt user for a product name
+        // return the product name
+
+        // placeholder
+        return null;
+    }
+
+    public Product getProductFromInventoryFile(String productName) throws IllegalArgumentException {
+        // parse the file
+        // check if any of the names are productName
+        // if they are, return the product, otherwise throw exception
+
+        // placeholder
+        return null;
+    }
+
+    public String getProductData(Product product){
+        // return the products overridden toString method that will be formatted properly with the data
+
+        // placeholder
+        return null;
+    }
+}
